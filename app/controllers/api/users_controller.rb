683ba9d5bd80a5934fd14cd749595af7f96f88e5
Alan Barton <abarton201@gmail.com>
2021-02-13 12:37:43 -0500

    add authentication to back end

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,18 @@ class Api::UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render "show.json.jb"
   end
+
+  def create
+    user = User.new(
+      user_name: params[:user_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
