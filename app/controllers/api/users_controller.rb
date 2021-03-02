a6fe86256716aeb9c97b075c9e34834d7ef94ebe
Alan Barton <abarton201@gmail.com>
2021-03-01 20:07:48 -0500

    add index user rought

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
   def show
     @user = User.find_by(id: params[:id])
     render "show.json.jb"
