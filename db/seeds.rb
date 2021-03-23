2e3c3d777f0bf0c87ac6311d659635541c9937e0
Alan Barton <abarton201@gmail.com>
2021-03-22 21:48:19 -0400

    update seeds folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-users = User.create!(user_name: "charcoal", email: "charlett@example.com", about_me: "i do things", password: "password", phone_number: "888-888-8888", profile_pic: "https://res.cloudinary.com/dnnf4debc/image/upload/v1615128857/zxdbnj4x9ywbumsh71yp.jpg")
-
 photo_ones = PhotoOne.create!(image: "https://res.cloudinary.com/dnnf4debc/image/upload/v1615044303/DSC_0770_hsbv6u.jpg", carousel: 1)
 photo_ones = PhotoOne.create!(image: "https://res.cloudinary.com/dnnf4debc/image/upload/v1615044302/DSC_0822_fierxv.jpg", carousel: 1)
 photo_ones = PhotoOne.create!(image: "https://res.cloudinary.com/dnnf4debc/image/upload/v1615044302/DSC_0177_vrare8.jpg", carousel: 1)
